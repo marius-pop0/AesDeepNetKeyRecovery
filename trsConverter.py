ed67e806fac699360b5cec19dd74ba007d50bc25
marius <marius.pop94@gmail.com>
2019-01-29 07:02:47 -0800

    get pt traces and ct in csv file

--- trsConverter.py
+++ trsConverter.py
@@ -1,21 +1,36 @@
 import pandas as pd
 import trsfile
+import binascii
 
 with trsfile.open('HWDES50000Traces.trs', 'r') as traces:
     # Show all headers
     for header, value in traces.get_headers().items():
         print(header, '=', value)
 
-    df = pd.DataFrame()
+    df_traces = pd.DataFrame()
+    df_data = pd.DataFrame()
 
     # Iterate over the traces
     for i, trace in enumerate(traces):
-        df = df.append(pd.DataFrame([trace.samples]), ignore_index=True)
-        # Print Trace and Info
+        df_traces = df_traces.append(pd.DataFrame([trace.samples]), ignore_index=True)
 
+        # Print Trace and Info
         # print(pd.DataFrame([trace.samples]))
         # print('Trace {0:d} contains {1:d} samples'.format(i, len(trace)))
         # print('  - minimum value in trace: {0:f}'.format(min(trace)))
         # print('  - maximum value in trace: {0:f}'.format(max(trace)))
 
-    df.to_csv("foo.csv", index=False)
+        # Print Data
+        # print(binascii.hexlify(trace.data).decode('utf8'))
+        # print("Plaintext " + binascii.hexlify(trace.data).decode('utf8')[0:16])
+        # print("Chipertext " + binascii.hexlify(trace.data).decode('utf8')[16:32])
+        data = binascii.hexlify(trace.data).decode('utf8')
+        df_data = df_data.append(pd.DataFrame([[data[0:16], data[16:32]]]), ignore_index=True)
+
+    print(df_data.head())
+
+    df_traces.insert(loc=0, column='pt', value=df_data[0])
+    df_traces['ct'] = df_data[1]
+
+    # Write to CSV file --> Takes a while to do for large traces
+    df_traces.to_csv("trace.csv", index=False)
